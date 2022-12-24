7fff399e54a04861c4657de1f563397d84e3e901
MahirDograr <MahirDograr@MAHIRSUPPORT>
2022-12-24 16:13:10 +0300

    Change Page

---
+++ BusinessLayer/ValidationRules/CategoryValidator.cs
@@ -0,0 +1,23 @@
+﻿using EntityLayer.Concrete;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.ValidationRules
+{
+    //fluentvalidation
+    public class CategoryValidator:AbstractValidator<Category>
+    {
+        public CategoryValidator()
+        {
+            RuleFor(x => x.CategoryName).NotEmpty().WithMessage("Kategori Adını Boş Geçemezsiniz.");
+            RuleFor(x => x.CategoryDescription).NotEmpty().WithMessage("Açıklama Boş Geçemezsiniz.");
+            RuleFor(x => x.CategoryName).MinimumLength(3).WithMessage("Kategori isminin minimum uzunluğu 3 olmalı.");
+            RuleFor(x => x.CategoryName).MaximumLength(20).WithMessage("Kategori isminin maksimum uzunluğu 20 olmalı.");
+
+        }
+    }
+}
